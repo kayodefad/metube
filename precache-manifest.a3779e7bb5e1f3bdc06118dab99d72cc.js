3b0501da3e5bf2f09b5c4774285605621ca43ec1
Oluwakayode <fadojuk@gmail.com>
2020-01-23 10:44:39 +0100

    Updates

---
+++ precache-manifest.a3779e7bb5e1f3bdc06118dab99d72cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "794f98d8d3337d61a42e30b7cda4a126",
+    "url": "/Metube/index.html"
+  },
+  {
+    "revision": "aa7f358d529f2d42adb2",
+    "url": "/Metube/static/css/main.f4e3bad1.chunk.css"
+  },
+  {
+    "revision": "87d69a858071f1437041",
+    "url": "/Metube/static/js/2.d23cdaaf.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Metube/static/js/2.d23cdaaf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa7f358d529f2d42adb2",
+    "url": "/Metube/static/js/main.f3340b7d.chunk.js"
+  },
+  {
+    "revision": "009cc81b3a5be3c1ef23",
+    "url": "/Metube/static/js/runtime-main.69976f9a.js"
+  }
+]);
