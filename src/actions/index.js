e4cf567aaafd7171436860dbe485612205321783
Oluwakayode <fadojuk@gmail.com>
2020-06-01 16:30:59 +0100

    Added new api key

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 export const addVideos = term => async dispatch => {
   const response = await axios.get(
-    `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&key=AIzaSyCgTXlMdNUMIYEo7WDVWbCacITH-yPQcfs&q=${term}`
+    `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&key=AIzaSyCoJa49vcHteYXZ-l7U4IIuZ0qM96vs_Qw&q=${term}`
   )
 
   dispatch({ type: ADD_VIDEOS, payload: response.data.items })
@@ -12,7 +12,7 @@ export const addVideos = term => async dispatch => {
 
 export const playVideo = (id) => async dispatch => {
   const response = await axios.get(
-    `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&key=AIzaSyCgTXlMdNUMIYEo7WDVWbCacITH-yPQcfs&q=${id}`
+    `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&key=AIzaSyCoJa49vcHteYXZ-l7U4IIuZ0qM96vs_Qw&q=${id}`
   )
 
   dispatch({ type: PLAY_VIDEO, payload: response.data.items })
@@ -22,3 +22,4 @@ export const playVideo = (id) => async dispatch => {
 // Api key 2: AIzaSyAci3hn1eTaPmMWo5pW-r5rpLslB5U5pNw
 // Api key 3: AIzaSyB5-3BfyxIUrsEne-E7Zj7uMd4_272xgTw
 // AIzaSyCgTXlMdNUMIYEo7WDVWbCacITH-yPQcfs
+//AIzaSyCoJa49vcHteYXZ-l7U4IIuZ0qM96vs_Qw
